60dd610a2e967e207a9ebe67c85f610feba8a878
GhitaB <ghitabizau27@gmail.com>
2021-06-07 10:57:26 +0300

    Add content.

---
+++ src/azsmr_scraper/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='azsmr_scraper',
+   version='1.0',
+   description='azsmr ic Scraper',
+   author='Ghita Bizau',
+   author_email='ghitabizau27@gmail.com',
+   packages=[],
+   install_requires=[],
+)
