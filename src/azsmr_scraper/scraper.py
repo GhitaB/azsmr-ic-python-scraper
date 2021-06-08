4738bcd2a462baef0b19ff8fc34f1c69a1823156
GhitaB <ghitabizau27@gmail.com>
2021-06-08 17:08:24 +0300

    WIP.

--- src/azsmr_scraper/scraper.py
+++ src/azsmr_scraper/scraper.py
@@ -1,2 +1,70 @@
+from bs4 import BeautifulSoup
+import os
+import requests
+import shutil
+
+data = {}
+
+all_urls = []
+
+
+def download_file(url, to_file):
+    user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36"
+    headers = {'User-Agent': user_agent}
+    with requests.get(url, headers=headers, stream=True) as r:
+        with open(to_file, 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+
+    return to_file
+
+
+def open_url(url):
+    user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36"
+    headers = {'User-Agent': user_agent}
+    page = requests.get(url, headers=headers)
+    return page
+
+
+def get_content(url):
+    page = open_url(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    ppp = soup.find_all('p', class_='post-title')
+
+    audio = "https://www.azsmr.ro" + soup.find_all("audio")[0].find_all("source")[0].attrs["src"]
+
+    filename = "aaaa.mp3"
+    audio_file = download_file(audio, filename)
+    breakpoint()
+    print(url)
+
+
+def get_pages_b(url):
+    page = open_url(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    ppp = soup.find_all('p', class_='post-title')
+
+    urls = [x.find_all("a")[0].attrs['href'] for x in ppp]
+
+    global all_urls
+    for uuu in urls:
+        all_urls.append(uuu)
+
+
 def main():
-    print("Done!")
+    # LEVEL A
+    # Example: http://www.azsmr.ro/imnuri-crestine/pagina/5
+    print("Level A. Start!")
+    url = "http://www.azsmr.ro/imnuri-crestine/pagina/"
+    pages_a = 2
+
+    # LEVEL B
+    # Example: https://www.azsmr.ro/5-cer-si-mare-si-pamant/
+    print("Level B. Start!")
+    for page in range(1, pages_a):
+        print("Level B. Progress...")
+        get_pages_b(url + str(page))
+
+    # LEVEL C
+    print("Level C. Start!")
+    for url in all_urls:
+        get_content(url)
